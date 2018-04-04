e0a6bb2de80cc72e70d1f154f8dd087630756d18
Hemesh-Unka <hemeshunka@gmail.com>
2018-04-04 18:04:07 +0100

    completed step 3 -> green

--- spec/note-list-view-tests.js
+++ spec/note-list-view-tests.js
@@ -3,6 +3,8 @@
     var list = new List();
 
     list.add(new Note('This is a new note'));
+    console.log(list)
+
     var view = new View(list);
     console.log('has the correct HTML format -', expect(view.getListView()).toEqual('<ul><li><div>This is a new note</div></li></ul>'));
   };
